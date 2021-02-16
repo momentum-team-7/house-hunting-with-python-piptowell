88f0b9f573b6f682e149d42eab63bd4e1fcc1089
Tip Powell <powelltip@gmail.com>
2021-02-16 10:47:18 -0500

    trying one more time

--- guessing-game.py
+++ guessing-game.py
@@ -2,16 +2,19 @@ import random
 
 player_one_number = random.randint(1,10)
 
-player_two_guess = int(input("Guess player one's number!"))
+player_two_guess = int(input("Guess player one's number: "))
+
+guess = 1
 
 while player_two_guess != player_one_number:
     if player_two_guess < player_one_number:
         print("You're low!")
-        player_two_guess = int(input("Wrong, try again:"))
+        player_two_guess = int(input("Try again: "))
+        guess = guess + 1
     elif player_two_guess > player_one_number:
         print("Youre high!")
-        player_two_guess = int(input("Wrong, try again:"))
-    else:
-        player_two_guess = player_one_number:
-        print("You are correct. See ya nerd.")
+        player_two_guess = int(input("Try again: "))
+
+
+print("You are correct. It only took you", guess,"guesses. See ya nerd.")
         
